03ee4ac2cac698257679501711f2fd73b2d503db
wooplix16-coder <wooplix16@gmail.com>
2026-03-25 10:17:12 +0530

    Update intentService.js

--- services/intentService.js
+++ services/intentService.js
@@ -1,12 +1,12 @@
 function detectIntent(message) {
   const msg = message.toLowerCase();
 
-  if (["hi", "hello", "hey"].some(g => msg.includes(g))) {
+  if (["hi", "hello", "hey", "good morning", "good evening"].some(g => msg.includes(g))) {
     return "greeting";
   }
 
-  if (msg.length < 3) {
-    return "unknown";
+  if (msg.trim().length < 2) {
+    return "empty";
   }
 
   return "query";
